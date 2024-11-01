7e1dcedf1599da733c1b300b683bfc57c8cf5007
eonist <30n1st@gmail.com>
2024-11-01 12:06:53 +0100

    Update SecTextView+Content.swift

--- Sources/SecureInput/ui/SecureTextView/util/SecTextView/SecTextView+Content.swift
+++ Sources/SecureInput/ui/SecureTextView/util/SecTextView/SecTextView+Content.swift
@@ -12,7 +12,7 @@ extension SecTextView {
     *               manages the changes in the text field 
     *               when the secure mode is toggled.
     */
-   internal var body: some View {
+   public var body: some View {
       textField
          .onChange(of: isSecured) { _, _ in // Callback for when text changes
             // set scrambled text or real value (keep this as a oneliner, makes the entire method use less vertical space etc)
